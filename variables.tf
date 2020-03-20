e4c23c2df53f1d06b1737e47f832f2fb4b666524
jyamaniha <jhonny.yamaniha@hotmail.com>
2020-03-20 11:49:29 -0500

    variables.tf via upload

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "iaas_classic_username" {
+  default = ""
+  description = "Usuario IBM Cloud IaaS por Cliente"
+}
+variable "ibmcloud_iaas_api_key" {
+  default = ""
+  description = "Key generado por Cliente"
+}
