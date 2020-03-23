77a9f9de7a656e74a4063c95fbb8ec361787d17c
jyamaniha <jhonny.yamaniha@hotmail.com>
2020-03-23 11:11:58 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,3 +6,28 @@ variable "ibmcloud_iaas_api_key" {
   default = ""
   description = "Key generado por Cliente"
 }
+
+variable "hostname" {
+  default = ""
+  description = "Nombre de hostname"
+}
+
+variable "domain" {
+  default = ""
+  description = "Nombre de dominio"
+}
+
+variable "cores" {
+  default = ""
+  description = "Numero de cores"
+}
+
+variable "memory" {
+  default = ""
+  description = ""
+}
+
+variable "private_subnet" {
+  default = ""
+  description = ""
+}
