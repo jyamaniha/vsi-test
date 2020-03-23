2141eeb1b914eeecb4275cf2b73d8a3127e481b2
jyamaniha <jhonny.yamaniha@hotmail.com>
2020-03-23 11:13:05 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,18 +3,18 @@ provider "ibm" {
   iaas_classic_api_key  = "${var.ibmcloud_iaas_api_key}"
 }
 resource "ibm_compute_vm_instance" "vm1" {
-  hostname             = "tfwin1"
-  domain               = "tfdemo.com"
+  hostname             = "${var.hostname}"
+  domain               = "${var.domain}"
   os_reference_code    = "WIN_2012-STD_64"
   datacenter           = "dal09"
   network_speed        = 100
   # Para cambiar de consumo por hora a mes poner hourly_billing = false
   hourly_billing       = true
   private_network_only = true
-  cores                = 1
-  memory               = 2048
-  disks                = [100]
+  cores                = "${var.cores}"
+  memory               = "${var.memory}"
+  disks                =  100
   local_disk           = false
 
-  private_subnet = "10.153.79.0/26"
+  private_subnet = "${var.private_subnet}"
 }
